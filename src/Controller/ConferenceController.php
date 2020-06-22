e3690cf64a6280d28bbab0591c0cd055798131bc
Robin Charlet <robincharlet@desktop-usjn8b8.home>
2020-06-22 15:51:19 +0200

    Create Conference Controller

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return new Response(<<<EOF
+        <html>
+            <body>
+                <img src="/images/under-construction.png" alt="Under Construction" width="300" height="200">
+            </body>
+        </html>
+EOF
+);
+    }
+}
