58c5b2804a23daf8bfdd239a9232ecb568d408ba
Robin Charlet <robincharlet@desktop-usjn8b8.home>
2020-06-23 16:20:20 +0200

    Add twig extensions bundle + show conference template + route

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
@@ -16,11 +21,31 @@ class ConferenceController extends AbstractController
      * @param ConferenceRepository $conferenceRepository
      *
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
         return new Response($twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll()
         ]));
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     * @param Environment       $twig
+     * @param Conference        $conference
+     * @param CommentRepository $commentRepository
+     *
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository) {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])]));
     }
 }
