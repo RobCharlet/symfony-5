a592c7f4e0f90f63c55369f2dfc2549175a7599e
Robin Charlet <robin.charlet@laposte.net>
2020-06-25 12:43:50 +0200

    Handle image uploading

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,7 +8,9 @@ use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -56,22 +58,41 @@ class ConferenceController extends AbstractController
 
     /**
      * @Route("/conference/{slug}", name="conference")
-     * @param Request              $request
-     * @param Conference           $conference
-     * @param ConferenceRepository $conferenceRepository
-     * @param CommentRepository    $commentRepository
+     * @param Request           $request
+     * @param Conference        $conference
+     * @param CommentRepository $commentRepository
+     * @param string            $photoDir
      *
      * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
+     * @throws Exception
      */
-    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository,CommentRepository $commentRepository) {
+    public function show(
+        Request $request,
+        Conference $conference,
+        CommentRepository $commentRepository,
+        // Value $photoDir injected in services.yaml
+        // (Symfony to inject the value whenever a service has a $photoDir argument.)
+        string $photoDir
+    )
+    {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+                $comment->setPhotoFilename($filename);
+            }
+
 
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
