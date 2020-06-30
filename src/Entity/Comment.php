3a8971208b1cf306789e0d814caf240cb14f11a4
Robin Charlet <robin.charlet@laposte.net>
2020-06-30 12:11:02 +0200

    Add Comment state column

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -54,6 +54,11 @@ class Comment
      */
     private $photoFilename;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -145,5 +150,17 @@ class Comment
     public function __toString(): string
     {
         return $this->getEmail();
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
     }
 }
