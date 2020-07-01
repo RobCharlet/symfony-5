ae76daeec120852d1b2478fee7ae350b932c2c28
Robin Charlet <robin.charlet@laposte.net>
2020-07-01 16:52:24 +0200

    Email test example

---
+++ tests/MessageHandler/CommentMessageHandlerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Tests\MessageHandler;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CommentMessageHandlerTest extends WebTestCase
+{
+/*    public function testMailerAssertions()
+    {
+        $client = static::createClient();
+        $client->request('GET',  '/');
+
+        $this->assertEmailCount(1);
+
+        $event = $this->getMailerEvent(0);
+        $this->assertEmailIsQueued($event);
+
+        $email = $this->getMailerMessage(0);
+        $this->assertEmailHeaderSame($email, 'to', 'admin@example.com');
+        $this->assertEmailTextBodyContains($email, 'Bar');
+        $this->assertEmailAttachmentCount($email, 1);
+    }
+    */
+}
