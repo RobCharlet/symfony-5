8b58fbcab7a544143b11e52b07543cf8c97293a3
Robin Charlet <robin.charlet@laposte.net>
2020-07-01 11:00:37 +0200

    Set workflow to Comment message handler

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -8,7 +8,10 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
 {
@@ -24,6 +27,18 @@ class CommentMessageHandler implements MessageHandlerInterface
      * @var CommentRepository
      */
     private $commentRepository;
+    /**
+     * @var MessageBusInterface
+     */
+    private $bus;
+    /**
+     * @var WorkflowInterface
+     */
+    private $workflow;
+    /**
+     * @var null|LoggerInterface
+     */
+    private $logger;
 
     /**
      * CommentMessageHandler constructor.
@@ -31,16 +46,25 @@ class CommentMessageHandler implements MessageHandlerInterface
      * @param EntityManagerInterface $entityManager
      * @param SpamChecker            $spamChecker
      * @param CommentRepository      $commentRepository
+     * @param MessageBusInterface    $bus
+     * @param WorkflowInterface      $commentStateMachine
+     * @param LoggerInterface|null   $logger
      */
     public function __construct(
         EntityManagerInterface $entityManager,
         SpamChecker $spamChecker,
-        CommentRepository $commentRepository
+        CommentRepository $commentRepository,
+        MessageBusInterface $bus,
+        WorkflowInterface $commentStateMachine,
+        LoggerInterface $logger = null
     )
     {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
         $this->commentRepository = $commentRepository;
+        $this->bus = $bus;
+        $this->workflow = $commentStateMachine;
+        $this->logger = $logger;
     }
 
     public function __invoke(CommentMessage $message)
@@ -51,10 +75,41 @@ class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+        // If the accept transition is available for the comment in the message, check for spam
+        if ($this->workflow->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+
+            // Depending on the outcome, choose the right transition to apply
+            if (2 === $score) {
+                $transition = 'reject_spam';
+            } elseif (1 === $score) {
+                $transition = 'might_be_spam';
+            }
+
+            // Call apply() to update the Comment via a call to the setState() method
+            $this->workflow->apply($comment, $transition);
+
+            // Call flush() to commit the changes to the database
+            $this->entityManager->flush();
+
+            // Re-dispatch the message to allow the workflow to transition again
+            $this->bus->dispatch($message);
+        }
+        //Will be handled by admin later
+        else if (
+            $this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')
+        ) {
+            $this->workflow->apply(
+                $comment,
+                $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham'
+            );
+            $this->entityManager->flush();
+        }
+
+
+        else if ($this->logger) {
+            $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
 
         $this->entityManager->flush();
